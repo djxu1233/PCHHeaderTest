884e51d6b2315bc89e79041c91f3b63967a984a4
DJ <djxu1233@163.com>
2018-12-13 16:31:01 +0800

    Initial PCHHeaderTest

---
+++ Sources/PCHHeaderTest.h
@@ -0,0 +1,23 @@
+//
+//  PCHHeaderTest.h
+//  PodTestDemo11111
+//
+//  Created by DJ on 2018/12/13.
+//  Copyright © 2018年 DJ. All rights reserved.
+//
+
+//#ifndef PCHHeaderTest_h
+//#define PCHHeaderTest_h
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Toast.
+FOUNDATION_EXPORT double PCHHeaderTestVersionNumber;
+
+//! Project version string for Toast.
+FOUNDATION_EXPORT const unsigned char PCHHeaderTestVersionString[];
+
+#import <PCHHeaderTest/UIView+DJRect.h>
+
+//#endif /* PCHHeaderTest_h */
+
